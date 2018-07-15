82b844978828b957b70ad3bed679552080873911
kevinrupert <kevinrupert.design@gmail.com>
2018-07-15 14:43:03 -0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$('.menu-icon').click(function() {
+  $('header').toggleClass('menu-opened');
+}
+);
+
+$('.animation-group').waypoint(function() {
+  $(this.element).find('.animate-fade, .animate-fade-up').addClass('animated');
+}, {offset: '90%' });
+
+$('.animate-fade, .animate-fade-up').waypoint(function() {
+  $(this.element).addClass('animated');
+}, {offset: '90%' });
+
+$('.dean-gabriele-intro').waypoint(function (direction) {
+    if (direction == 'down') {
+        $('.dean-gabriele-image').addClass('stick');
+    } else {
+        $('.dean-gabriele-image').removeClass('stick');
+    }
+}, {offset: '0px' });
+  
+  
